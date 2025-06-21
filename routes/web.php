0d65c48640ef5f7f61f8efa78615f4b79c5451a6
lesl1602 <144073942+lesl1602@users.noreply.github.com>
2025-06-20 20:57:05 -0600

    rutas basicas (GET, POST, PUT, DELETE)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas get y post
+Route::get('/saludo', function () {
+    return "¡Hola Mundo desde una ruta básica!";
+})->name('saludo.basico');
+
+Route::get('/formulario', function () {
+    return '<form action="/contacto" method="POST">' . csrf_field() . '<button type="submit">Enviar Petición POST</button></form>';
+})->name('formulario.mostrar');
+
+Route::post('/contacto', function () {
+    return "Petición POST recibida correctamente.";
+})->name('contacto.enviar');
+
+// PUT, PATCH Y DELETE
+
+// Ruta para actualizar un recurso
+Route::put('/recurso/actualizar', function () {
+    return "Recurso actualizado con PUT.";
+})->name('recurso.actualizar');
+
+// Ruta para eliminar un recurso
+Route::delete('/recurso/eliminar', function () {
+    return "Recurso eliminado con DELETE.";
+})->name('recurso.eliminar');
